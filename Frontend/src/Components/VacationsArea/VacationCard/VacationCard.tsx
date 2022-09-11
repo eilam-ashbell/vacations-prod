bf243894983b5373dd6aec81eba2c00c2a1597c7
eilam-ashbell <eashbell@gmail.com>
2022-09-12 00:27:59 +0300

    admin btns on cards

--- Frontend/src/Components/VacationsArea/VacationCard/VacationCard.tsx
+++ Frontend/src/Components/VacationsArea/VacationCard/VacationCard.tsx
@@ -3,7 +3,12 @@ import FollowBtn from "../FollowBtn/FollowBtn";
 import "./VacationCard.css";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import VacationForUserModel from "../../../Models/vacationForUserModel";
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { Button } from "@mui/material";
+import { color } from "@mui/system";
+import { VacationsAction, VacationsActionType, vacationsStore } from "../../../Redux/VacationsState";
+import { authStore } from "../../../Redux/AuthState";
 
 interface VacationCardProps {
     vacationData: VacationForUserModel;
@@ -21,19 +26,70 @@ function VacationCard(props: VacationCardProps): JSX.Element {
         backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.7), rgba(255, 255, 255, 0)60%), url(${config.serverStaticsImages + props.vacationData.imageName})`
     }
 
+    const adminBtnStyle = {
+        backgroundColor: "white",
+        color: "#9c9496",
+        boxShadow: "none",
+        fontSize: 12,
+        textTransform: "capitalize",
+        fontFamily: 'roboto',
+        borderRadius: "100px",
+        '&:hover': {
+            backgroundColor: '#9c9496',
+            color: '#fff',
+            borderColor: '#0062cc',
+            boxShadow: 'none',
+        },
+    }
+
+    const adminBtnIconsStyle = {
+        width: "14px",
+        transform: "translateY(-1px)"
+    }
+
+    function handleDelete() {
+        const action: VacationsAction = {
+            type: VacationsActionType.DeleteVacation,
+            payload: props.vacationData.vacationId
+        }
+        vacationsStore.dispatch(action)
+    }
+
     return (
         <div className="VacationCard">
-            <div 
-            className="bg-image"
-            style={bgImageStyle} 
+            <div
+                className="bg-image"
+                style={bgImageStyle}
             >
-                <FollowBtn vacation={props.vacationData}/>
+                <FollowBtn vacation={props.vacationData} />
+                {
+                    authStore.getState().user.roleId === 1 &&
+                    <div className="admin-wrapper">
+                        <Button
+                            variant="contained"
+                            size="small"
+                            startIcon={<DeleteIcon sx={adminBtnIconsStyle} />}
+                            sx={adminBtnStyle}
+                            onClick={handleDelete}>
+                            Delete
+                        </Button>
+                        <Button
+                            variant="contained"
+                            size="small"
+                            startIcon={<EditIcon fontSize="small" sx={adminBtnIconsStyle} />}
+                            sx={adminBtnStyle}
+                        // onClick={}
+                        >
+                            Edit
+                        </Button>
+                    </div>
+                }
                 <h3>{props.vacationData.destination}</h3>
             </div>
             <div className="card-body">
                 <span className="vacation-dates"><span className="from-date">{formatDate(props.vacationData.startDate)}</span>
-                <KeyboardArrowRightIcon/>
-                <span className="till-date">{formatDate(props.vacationData.endDate)}</span></span>
+                    <KeyboardArrowRightIcon />
+                    <span className="till-date">{formatDate(props.vacationData.endDate)}</span></span>
                 <hr></hr>
                 <p className="vacation-description">{props.vacationData.description}
                 </p>
--- Frontend/src/Components/VacationsArea/Vacations/Vacations.tsx
+++ Frontend/src/Components/VacationsArea/Vacations/Vacations.tsx
@@ -8,11 +8,13 @@ import jwtDecode from "jwt-decode";
 import { authStore } from "../../../Redux/AuthState";
 import UserModel from "../../../Models/userModel";
 import { vacationsStore } from "../../../Redux/VacationsState";
+import { useBadge } from "@mui/base";
+import config from "../../../Utils/Config";
 
 
 function Vacations(): JSX.Element {
 
-    const pageSize = 5
+    const pageSize = config.numOfVacationsOnPage
     const [vacations, setVacations] = useState<VacationForUserModel[]>([])
     const [vacationsToDisplay, setVacationsToDisplay] = useState<VacationForUserModel[]>([])
     const [pagination, setPagination] = useState({
@@ -21,28 +23,48 @@ function Vacations(): JSX.Element {
         to: pageSize
     })
 
-
+    // Get all vacation for specific user on load
     useEffect(() => {
+        // Extract user object from token
         const container: { user: UserModel } = jwtDecode(authStore.getState().token)
         const user = container.user
+        // Get vacation for this user
         vacationsService.getAllVacations(user.userUuid).then(result => {
+            // Set results in local state
             setVacations(result)
         })
-
-        // const unsubscribe = vacationsStore.subscribe(() => {
-        //     setVacations(vacationsStore.getState().vacations)            
-        // })
-
-
     }, [])
 
     useEffect(() => {
+
+        // Define how many vacations there are in the state
         const vacationsLength = vacations.length
+
+        // Define which vacation to display
         const vacationsOnPage = vacations.slice(pagination.from, pagination.to)
+
+        // Set vacation count in state
         setPagination({ ...pagination, count: vacationsLength })
+
+        // Set relevant vacation to display
         setVacationsToDisplay(vacationsOnPage)
+
+        // If vacation store changed > repeat functionality
+        const unsubscribe = vacationsStore.subscribe(() => {
+            setVacations(vacationsStore.getState().vacations)
+            const vacationsLength = vacations.length
+            const vacationsOnPage = vacations.slice(pagination.from, pagination.to)
+            setPagination({ ...pagination, count: vacationsLength })
+            setVacationsToDisplay(vacationsOnPage)
+        })
+
+        // Unsubscribe from vacation store
+        return () => unsubscribe()
+
+        // Repeat functionality each time local states are change
     }, [vacations, pagination.from, pagination.to])
 
+    // Handle page change on pagination
     function handlePageChange(event: React.ChangeEvent<unknown>, page: number): void {
         const from = (page - 1) * pageSize;
         const to = (page - 1) * pageSize + pageSize;
@@ -51,13 +73,14 @@ function Vacations(): JSX.Element {
 
     return (
         <div className="Vacations">
-            <div className="vacations-wrapper">
-                {
-                    vacationsToDisplay.map(v =>
-                        <VacationCard key={v.vacationId} vacationData={v} />
-                    )
-                }
-
+            <div className="vacations-align">
+                <div className="vacations-wrapper">
+                    {
+                        vacationsToDisplay.map(v =>
+                            <VacationCard key={v.vacationId} vacationData={v} />
+                        )
+                    }
+                </div>
             </div>
             <div className="pagination-wrapper">
                 <Pagination
