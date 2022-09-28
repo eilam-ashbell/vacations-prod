5f8b5ffd4f1f65a9f5f177b2307ad43610e4f58e
eilam-ashbell <eashbell@gmail.com>
2022-09-28 23:37:29 +0300

    Revert "design"
    This reverts commit 25fbc15d2403b4e29de0fb3fd972255162ffd399.

--- Backend/src/4-models/role-model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RoleModel = /** @class */ (function () {
-    function RoleModel() {
-    }
-    return RoleModel;
-}());
-exports.default = RoleModel;
